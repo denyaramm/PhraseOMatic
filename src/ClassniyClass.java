9d3780bcd4771a3a07206f7d6334f15265928fdc
TimurBattalkhanov <32982157+TimurBattalkhanov@users.noreply.github.com>
2018-09-19 15:14:33 +0600

    Внесение скрытой реализации

--- src/ClassniyClass.java
+++ src/ClassniyClass.java
@@ -1,5 +1,14 @@
 public class ClassniyClass {
-    public static void main(String[] args) {
-        System.out.printf("Кляссный такой клясс получился");
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+    public int AgeCount(int b) {
+        return AgeIncrement(b);
+    }
+    private int AgeIncrement(int a) {
+        this.age = a;
+        return a + 1;
     }
 }
